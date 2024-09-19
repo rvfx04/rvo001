1467cfdbdfda6b00c794f63e5a48e2061e12c990
rvfx04 <rvfx04@gmail.com>
2024-09-19 17:57:33 -0500

    Update pruegantt1809.py

--- pruegantt1809.py
+++ pruegantt1809.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 
 # Crear el DataFrame original (Ejemplo de un solo pedido)
 data = {
-    'PEDIDO': [1198],
+   'PEDIDO': [1198],
     'IdDocumento': [441563],
     'F_EMISION': ['2024-06-20'],
     'F_ENTREGA': ['2024-09-06'],
@@ -17,6 +17,16 @@ data = {
     'KG_TELAPROBP': ['102%'],
     'CORTADOP': ['113%'],
     'COSIDOP': ['19%'],
+    'FMINARM': ['2024-06-25'],
+    'FMAXARM': ['2024-07-05'],
+    'FMINTENID': ['2024-06-28'],
+    'FMAXTENID': ['2024-07-20'],
+    'FMINTELAPROB': ['2024-07-12'],
+    'FMAXTELAPROB': ['2024-09-06'],
+    'FMINCORTE': ['2024-07-17'],
+    'FMAXCORTE': ['2024-09-09'],
+    'FMINCOSIDO': ['2024-08-08'],
+    'FMAXCOSIDO': ['2024-09-12']
 }
 
 df = pd.DataFrame(data)
